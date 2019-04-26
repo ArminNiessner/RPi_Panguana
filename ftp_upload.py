d6da57d29bc75ba7f7e62df7563a3c403a91fdd1
ANiess <armin.niessner@googlemail.com>
2019-04-26 10:53:15 +0200

    misc edit

--- ftp_upload.py
+++ ftp_upload.py
@@ -1,10 +1,27 @@
+
+ # Copyright (C) 2019  Armin Niessner
+ 
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ 
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ 
+ # You should have received a copy of the GNU General Public License
+ # along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 from ftplib import FTP
 #from time import sleep
 
 def ftp_upload():
-    server = 'ftp.taysira.org'
-    username = 'aniessner@taysira.org'
-    password = 'e2m3f4meA?'
+    server = 'ftp.name.org'
+    username = 'username'
+    password = 'password'
 
     try:
         ftp_connection = FTP(server, username, password)
